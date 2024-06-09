ddaba50aee32863c3d9b8da642ec0663c8214d3d
NguyenVuong22 <vanvuong2210@gmail.com>
2024-06-09 14:41:42 +0700

    Exercise - function is_number

---
+++ Is_number.py
@@ -0,0 +1,11 @@
+import math
+def is_number(n):
+  try:
+    float(n)
+  except ValueError:
+    return False
+  return True
+assert is_number(3) == 1.0
+assert is_number('-2a') == 0.0
+print(is_number(1))
+print(is_number('n'))
