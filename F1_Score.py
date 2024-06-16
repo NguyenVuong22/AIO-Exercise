1ace8b2873d0fc390d94ab4ba8101c4b8a9106b9
NguyenVuong22 <vanvuong2210@gmail.com>
2024-06-16 09:28:43 +0700

    delete homework for week 2

--- F1_Score.py
+++
@@ -1,9 +0,0 @@
-# đánh giá classification model bằng F1-Score
-import math
-def calc_f1_score(tp, fp, fn):
-  Precision = tp/(tp + fp)
-  Recall = tp / (tp + fn)
-  F1_score = 2*(Precision * Recall)/(Precision + Recall)
-  return F1_score
-assert round(calc_f1_score(tp = 2, fp = 3, fn = 5), 2) == 0.33
-print(round(calc_f1_score(tp = 2, fp = 3, fn = 5), 2))
